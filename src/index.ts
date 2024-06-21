5da23c5ffc0264a45d9101f0c555b21dd94dcf1b
lorfly4 <87179865+lorfly4@users.noreply.github.com>
2024-06-21 07:01:45 +0700

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -99,6 +99,178 @@ app.get('/rpl/:id', async ({ params, set }) => {
   }
 });
 
+app.get('/matematika', ({ set }) => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM `matematika`', (err: QueryError | null, results: User[]) => {
+      if (err) {
+        set.status = 500;
+        reject({ error: err });
+      } else {
+        set.status = 200;
+        resolve({
+          message: 'Data berhasil diambil',
+          users: results
+        });
+      }
+    });
+  });
+});
+
+app.get('/matematika/:id', async ({ params, set }) => {
+  const { id } = params;
+
+  try {
+    // Query SQL untuk select data berdasarkan ID
+    const [rows] = await connection.promise().query<mapel[]>('SELECT * FROM matematika WHERE id = ?', [id]);
+
+    if (rows.length > 0) {
+      // Output data dari setiap baris
+      const data = rows[0];
+      set.status = 200;
+      return {
+        message: 'Data berhasil diambil',
+        data: data
+      };
+    } else {
+      set.status = 404;
+      return { message: 'Data tidak ditemukan' };
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    set.status = 500;
+    return { error: 'Terjadi kesalahan dalam server' };
+  }
+});
+
+app.get('/agama', ({ set }) => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM `pendidikan_agama`', (err: QueryError | null, results: User[]) => {
+      if (err) {
+        set.status = 500;
+        reject({ error: err });
+      } else {
+        set.status = 200;
+        resolve({
+          message: 'Data berhasil diambil',
+          users: results
+        });
+      }
+    });
+  });
+});
+
+app.get('/agama/:id', async ({ params, set }) => {
+  const { id } = params;
+
+  try {
+    // Query SQL untuk select data berdasarkan ID
+    const [rows] = await connection.promise().query<mapel[]>('SELECT * FROM pendidikan_agama WHERE id = ?', [id]);
+
+    if (rows.length > 0) {
+      // Output data dari setiap baris
+      const data = rows[0];
+      set.status = 200;
+      return {
+        message: 'Data berhasil diambil',
+        data: data
+      };
+    } else {
+      set.status = 404;
+      return { message: 'Data tidak ditemukan' };
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    set.status = 500;
+    return { error: 'Terjadi kesalahan dalam server' };
+  }
+});
+
+app.get('/ipa', ({ set }) => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM `ipa`', (err: QueryError | null, results: User[]) => {
+      if (err) {
+        set.status = 500;
+        reject({ error: err });
+      } else {
+        set.status = 200;
+        resolve({
+          message: 'Data berhasil diambil',
+          users: results
+        });
+      }
+    });
+  });
+});
+
+app.get('/ipa/:id', async ({ params, set }) => {
+  const { id } = params;
+
+  try {
+    // Query SQL untuk select data berdasarkan ID
+    const [rows] = await connection.promise().query<mapel[]>('SELECT * FROM ipa WHERE id = ?', [id]);
+
+    if (rows.length > 0) {
+      // Output data dari setiap baris
+      const data = rows[0];
+      set.status = 200;
+      return {
+        message: 'Data berhasil diambil',
+        data: data
+      };
+    } else {
+      set.status = 404;
+      return { message: 'Data tidak ditemukan' };
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    set.status = 500;
+    return { error: 'Terjadi kesalahan dalam server' };
+  }
+});
+
+app.get('/pkk', ({ set }) => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM `bahasa_inggris`', (err: QueryError | null, results: User[]) => {
+      if (err) {
+        set.status = 500;
+        reject({ error: err });
+      } else {
+        set.status = 200;
+        resolve({
+          message: 'Data berhasil diambil',
+          users: results
+        });
+      }
+    });
+  });
+});
+
+app.get('/pkk/:id', async ({ params, set }) => {
+  const { id } = params;
+
+  try {
+    // Query SQL untuk select data berdasarkan ID
+    const [rows] = await connection.promise().query<mapel[]>('SELECT * FROM pkk WHERE id = ?', [id]);
+
+    if (rows.length > 0) {
+      // Output data dari setiap baris
+      const data = rows[0];
+      set.status = 200;
+      return {
+        message: 'Data berhasil diambil',
+        data: data
+      };
+    } else {
+      set.status = 404;
+      return { message: 'Data tidak ditemukan' };
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    set.status = 500;
+    return { error: 'Terjadi kesalahan dalam server' };
+  }
+});
+
 // Login route
 app.post('/login', async ({ body, set }) => {
   const { username, password } = body as { username: string, password: string };
@@ -126,7 +298,7 @@ app.post('/login', async ({ body, set }) => {
 
 
 app.post('/register', ({ body, set }) => {
-  const { username, password, nama, kelas } = body as { username: string, password: string, nama: string, kelas: string };
+  const { username, password, nama } = body as { username: string, password: string, nama: string };
 
   return new Promise((resolve, reject) => {
     // Periksa apakah username sudah digunakan
@@ -148,8 +320,8 @@ app.post('/register', ({ body, set }) => {
 
         // Jika username belum digunakan, lakukan registrasi
         connection.query(
-          'INSERT INTO `users` (`username`, `password`, `nama`, `kelas`) VALUES (?, ?, ?, ?)',
-          [username, password, nama, kelas],
+          'INSERT INTO `users` (`username`, `password`, `nama`) VALUES (?, ?, ?)',
+          [username, password, nama],
           (err) => {
             if (err) {
               set.status = 500;
